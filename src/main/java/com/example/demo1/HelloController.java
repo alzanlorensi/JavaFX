357c49f2c771c8c38408ce8b74c50f6aeff2170a
Andre Zanlorensi <andrezanlorensi@hotmail.com>
2022-10-13 10:51:49 -0300

    Primeiro Commit aqui eu fiz um botão com um evento onde clicando no botão teste de evento ele mostra um click no console

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo1;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -10,5 +11,9 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    private Button teste_de_evento;
+    public void onteste_de_eventoAction(){
+        System.out.println("Click");
     }
 }
