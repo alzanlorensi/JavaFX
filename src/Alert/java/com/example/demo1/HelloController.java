ac553fb7abe8cd487eb5bbff9db84861339df29b
Andre Zanlorensi <andrezanlorensi@hotmail.com>
2022-10-13 11:02:01 -0300

    Copia do exercicio anterior

---
+++ src/Alert/java/com/example/demo1/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Applicationn!");
+    }
+    private Button teste_de_evento;
+    public void onteste_de_eventoAction(){
+        System.out.println("Click");
+    }
+}
